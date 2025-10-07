4d74e18ff2ea82eee028959453c54cced8ce82c9
Gabriel Santos <132019814+gssilva11@users.noreply.github.com>
2025-10-07 21:31:48 +0000

    v0.0.2

--- fasternet/src/supabaseClient.js
+++ fasternet/src/supabaseClient.js
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  console.error("Supabase URL ou KEY não encontradas nas variáveis de ambiente");
-}
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
